a84271c90679030a7b702ea66d8b1e8a95622c70
Fahad Murtaza <fahdim@gmail.com>
2017-05-06 21:50:28 +0500

    Using the mocked service.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,18 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import { HeroService } from "../hero.service"
 
 @Component({
   selector: 'app-heroes',
@@ -22,12 +10,19 @@ const HEROES: Hero[] = [
 
 export class HeroesComponent implements OnInit {
   title = 'Tour of Heroes';
-  heroes = HEROES;
   selectedHero: Hero;
+  heroes: Hero[];
 
-  constructor() { }
+  constructor(private heroService: HeroService) { 
+
+  }
 
   ngOnInit() {
+     this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 
   onSelect(hero:  Hero): void {
